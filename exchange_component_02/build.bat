ffb233ea557eda96b1eb0b82bd75221fc251516a
Jonathan Carle <jonathanleocarle@gmail.com>
2017-02-20 10:45:39 +0100

    Added IBaggageInterfaces for Jan

--- exchange_component_02/build.bat
+++ exchange_component_02/build.bat
@@ -1,8 +1,8 @@
-"%JAVA%\bin\javac" -Xlint:all -g:none -classpath build -d build src\IComponent.java
+"%JAVA%\bin\javac" -Xlint:all -g:none -classpath build -d build src\IBaggageScanner.java
 "%JAVA%\bin\javac" -Xlint:all -g:none -classpath build -d build src\Component.java
 
 cd build
-"%JAVA%\bin\jar" -cvf Component.jar IComponent.class
+"%JAVA%\bin\jar" -cvf Component.jar IBaggageScanner.class
 "%JAVA%\bin\jar" -uvf Component.jar Component.class
 "%JAVA%\bin\jar" -uvf Component.jar Component$Port.class
 
---
+++ exchange_component_02/src/IBaggageScanner.java
@@ -0,0 +1,3 @@
+public interface IBaggageScanner {
+    public boolean search(String text);
+}
