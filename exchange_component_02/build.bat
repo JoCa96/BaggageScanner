144b8701de80436dc1f7c4b0493044614f9e9c75
Jonathan Carle <jonathanleocarle@gmail.com>
2017-02-20 12:42:13 +0100

    Application.java

--- exchange_component_02/build.bat
+++ exchange_component_02/build.bat
@@ -2,10 +2,10 @@
 "%JAVA%\bin\javac" -Xlint:all -g:none -classpath build -d build src\KnuthMorrisPratt.java
 
 cd build
-"%JAVA%\bin\jar" -cvf KnuthMorrisPratt.jar IBaggageScanner.class
-"%JAVA%\bin\jar" -uvf KnuthMorrisPratt.jar KnuthMorrisPratt.class
-"%JAVA%\bin\jar" -uvf KnuthMorrisPratt.jar KnuthMorrisPratt$Port.class
+"%JAVA%\bin\jar" -cvf Component.jar IBaggageScanner.class
+"%JAVA%\bin\jar" -uvf Component.jar KnuthMorrisPratt.class
+"%JAVA%\bin\jar" -uvf Component.jar KnuthMorrisPratt$Port.class
 
-move KnuthMorrisPratt.jar ..\KnuthMorrisPratt.jar
+move Component.jar ..\Component.jar
 
 pause
